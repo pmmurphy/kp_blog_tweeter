01a15a743f9b085becc6b329adf189cd73d8dcff
Paula Murphy <paulam849@gmail.com>
2016-01-06 14:33:10 -0500

    added signup button

--- app.rb
+++ app.rb
@@ -3,3 +3,31 @@ require "sinatra/activerecord"
 require "./models"
 
 set :database, "sqlite3:tweeterdb.sqlite3"
+
+enable :sessions
+
+get '/' do
+	erb:'sign-in', :layout => false
+end	
+
+
+post '/sign-in' do
+	@user = User.where(username: params[:username]).last
+
+	if @user && @user.password == params[:password]
+		# session[:user_id] 
+		# flash[:notice] = "You are signed in"
+		redirect '/'
+
+	else
+		redirect '/sign-in'
+	end
+end
+
+get '/signup' do
+	erb:'signup', :layout => false
+end	
+
+
+
+
