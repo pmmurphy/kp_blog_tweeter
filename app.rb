d52f0c6b6fa1af536fe1961b6d7480be5cada455
Kayla Williams <kkayla.williams@icloud.com>
2016-01-06 18:11:51 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,39 @@
+# app.rb
+require "sinatra"
+require "sinatra/activerecord"
+
+set :database, "sqlite3:myblogdb.sqlite3"
+
+require "./models"
+
+require "sinatra/flash"
+
+enable :session
+
+get '/' do
+
+	Post.create(title: "Hello", body: "This is a post")
+	@posts = Post.all
+	erb :index
+end
+
+get '/post' do
+	@last_post = Post.last
+	erb :post_page
+end
+
+get '/sign-in' do
+	erb :signin
+end
+
+post '/sign-in' do
+	@user = User.where(username: params[:username]).first
+
+	if @user && @user.password == params[:password]
+		session[:user_id] = @user.id
+		flash[:notice] =  "Welcome! you're now signed in"
+	else
+		flash[:notice] = "Woops! There was a little hiccup"
+		redirect '/'
+ end
+end
